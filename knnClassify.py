f139c2fe4321f20c2bca3dcc134e44bc7881044c
Rob Hochberg <hochberg@udallas.edu>
2020-02-21 11:01:11 -0600

    add knnClassify.py for k nearest neighbor classification.

--- knnClassify.py
+++ knnClassify.py
@@ -171,7 +171,7 @@ def slantiness(img):
 
 # David/Sri/Michael
 # Returns the convex hull as a list of points.
-def convex_hull(img: np.array) -> list:
+def convex_hull(img):
     # Convert the image to an OpenCV-compatible format.
     compat_image = np.uint8(img)
     # Threshold the image.
@@ -259,7 +259,6 @@ def Hough_circles(img):
     return sectional_density(hough[0])
 
 
-
 """
     Admin Functions
 """
@@ -296,11 +295,16 @@ def build_feature_map(digit_map, fnlist):
 # Find center of mass of each digit's feature vectors
 # feature_map is a map from each digit to a list of feature vectors
 # Returns a map: digit -> Center of mass
-def find_com(feature_map):
+def find_coms(feature_map):
+    num_centers = 2
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
     com_map = {}
     for digit in feature_map:
         feature_matrix = np.asfarray(feature_map[digit])
-        com_map[digit] = np.mean(feature_matrix, axis=0)
+        #com_map[digit] = np.mean(feature_matrix, axis=0)
+        ret,label,centers = cv2.kmeans(np.asarray(feature_map[digit], dtype='float32'), num_centers, \
+                    None, criteria, 10,cv2.KMEANS_RANDOM_CENTERS)
+        com_map[digit] = centers
     return com_map
 
 
@@ -335,10 +339,11 @@ def testAMD(feature_map, com_map):
         for ele in feature_map[key]:
             mdist = float("inf")
             for ckey in com_map:
-                dist = abs(np.linalg.norm(com_map[ckey]-ele))
-                if dist <= mdist:
-                    mdist = dist
-                    ccom = ckey
+                for com in com_map[ckey]:
+                    dist = abs(np.linalg.norm(com-ele))
+                    if dist < mdist:
+                        mdist = dist
+                        ccom = ckey
             if ccom == key:
                 countr +=1  
             else:
@@ -358,11 +363,12 @@ def testSPM(feature_map, com_map):
         for some_features in list_of_features:
             smallest_distance = float("inf")
             guess_digit = None
-            for candidate_digit, a_com in com_map.items():
-                distance = np.linalg.norm(a_com - some_features)
-                if distance < smallest_distance:
-                    smallest_distance = distance
-                    guess_digit = candidate_digit
+            for candidate_digit, com_list in com_map.items():
+                for a_com in com_list:
+                    distance = np.linalg.norm(a_com - some_features)
+                    if distance < smallest_distance:
+                        smallest_distance = distance
+                        guess_digit = candidate_digit
             if guess_digit == correct_digit:
                 num_correct += 1
             num_total += 1
@@ -378,19 +384,22 @@ def testR(feature_map, com_map):
         for some_features in list_of_features:
             smallest_distance = float("inf")
             guess_digit = None
-            for candidate_digit, a_com in com_map.items():
-                distance = np.linalg.norm(a_com - some_features)
-                if distance < smallest_distance:
-                    smallest_distance = distance
-                    guess_digit = candidate_digit
+            for candidate_digit, com_list in com_map.items():
+                for a_com in com_list:
+                    distance = np.linalg.norm(a_com - some_features)
+                    if distance < smallest_distance:
+                        smallest_distance = distance
+                        guess_digit = candidate_digit
             predictions[correct_digit][guess_digit] += 1
     return predictions
 
 # List of implemented feature functions
 all_features = [waviness, hv_weights, top_bottom_balance, combineWavy,\
-                vertical_lines, sectional_density, slantiness, Hough_circles,\
-                edginess]
+                vertical_lines, sectional_density, slantiness,\
+                edginess, Sobelness]
 all_features = [waviness]
+
+
 
 data = read_images("data/mnist_medium.csv")
 digit_map = make_digit_map(data)
@@ -402,33 +411,43 @@ for f in all_features:
     
     # train
     feature_map = build_feature_map(digit_map, features)
-    com = find_com(feature_map)
 
-    # Test on training data
-    print("AMD Test", testAMD(feature_map, com))
-    print("SPM Test", testSPM(feature_map, com))
-    print( "R Test\n", np.array(testR(feature_map, com)))
+    train = []
+    labels = []
+    for digit in range(10):
+        for f in feature_map[digit]:
+            train.append(f)
+            labels.append(digit)
+    print(train)
+    print(labels)
+    
+    knn = cv2.ml.KNearest_create()
+    knn.train(np.array(train).astype(np.float32), cv2.ml.ROW_SAMPLE, np.array(labels).astype(np.float32))
 
     # Test on test data
     data = read_images("data/mnist_medium_test.csv")
     digit_map = make_digit_map(data)
     feature_map = build_feature_map(digit_map, features)
-    print("AMD Test", testAMD(feature_map, com))
-    print("SPM Test", testSPM(feature_map, com))
-    print( "R Test\n", np.array(testR(feature_map, com)))
 
-           
+    success = 0.0
+    failure = 0.0
+    predictions = [[0 for i in range(10)] for j in range(10)]
+    for digit in range(10):
+        for f in feature_map[digit]:
+            unkn = np.array([f]).astype(np.float32)
+            ret, results, neighbors ,dist = knn.findNearest(unkn, 3)
+            prediction = results[0][0]
+            predictions[digit][int(prediction)] += 1
+            if prediction == digit:
+                success += 1
+            else:
+                failure += 1
+    print(success/(success + failure))
+    print(np.array(predictions))
 
-"""
-data = read_images("mnist_medium.csv")
-digit_map = make_digit_map(data)
-
-for img in digit_map[8]:
-    imgr = img.reshape((image_size*1, image_size/1))
-    plt.imshow(imgr, cmap=plt.cm.binary)
-    plt.show()
-"""
-
+    
+    
+    
 
 
            

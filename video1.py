8dd9770ed85c7d9a2aad4eb83ff24c3420ffac0e
Rob Hochberg <csudallas@gmail.com>
2020-02-24 11:43:18 -0600

    add sudoku board

--- video1.py
+++ video1.py
@@ -2,15 +2,15 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread("../sudoku.png", cv2.IMREAD_GRAYSCALE)
+img_orig = cv2.imread("../sudoku.png", cv2.IMREAD_GRAYSCALE)
 
 # Show the original image
 # This is a matplotlib display, so we must close the window to move forward
-plt.imshow(255-img, cmap=plt.cm.binary)
+plt.imshow(255-img_orig, cmap=plt.cm.binary)
 plt.show()
 
 element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7), (3, 3))
-img = 255-cv2.dilate(255-img, element)
+img = 255-cv2.dilate(255-img_orig, element)
 # This is a matplotlib display, so we must close the window to move forward
 plt.imshow(255-img, cmap=plt.cm.binary)
 plt.show()
@@ -43,19 +43,19 @@ im_with_keypoints = cv2.drawKeypoints(img, keypoints, None, (255,0,255),\
 cv2.imshow("Keypoints", im_with_keypoints)
 cv2.waitKey(0)
 
-"""
+
 for k in keypoints:
     size = int(k.size)
     p = tuple(int(x-size/2) for x in k.pt)
     print(p)
-    #cv2.rectangle(img, p, (p[0]+size, p[1]+size), 200, 5)
+    cv2.rectangle(img, p, (p[0]+size, p[1]+size), 200, 5)
 
-    digit_img = img[p[1]:p[1]+size, p[0]:p[0]+size]
+    digit_img = img_orig[p[1]:p[1]+size, p[0]:p[0]+size]
     cv2.imshow("One Digit", digit_img)
     while cv2.waitKey(1) & 0xFF != ord('n'):
         continue
 cv2.imshow("Blob Rectangles", img)
-"""
+
 
 
 cap = cv2.VideoCapture(0)

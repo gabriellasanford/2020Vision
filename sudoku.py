fbe35126819dbba5cca5afbbc9d9458c8afe586a
Yeabkal <ywubshit@udallas.edu>
2020-03-08 19:22:01 -0500

    Support both PySAT and command line minisat

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,15 @@
-# Yeabkal Wubshit
-# Sudoku Solver using Backtracking and SAT Solver.
+'''
+Yeabkal Wubshit
+Sudoku Solver using Backtracking and SAT Solver.
+
+*** REQUIREMENTS ***
+To solve a Sudoku puzzle through a SAT solver, you need to have either:
+ 1) PySAT module installed (https://pypi.org/project/python-sat/) [PREFERRED WAY]
+ 2) `minisat` command line tool 
+
+'''
+
+PYSAT_SOLVER_INSTALLED = True
 
 import math
 from os import listdir
@@ -7,17 +17,26 @@ from os.path import isfile, join
 import time
 import random
 from os import system
-from pysat.solvers import Glucose3
+try:
+    from pysat.solvers import Glucose3
+except:
+    PYSAT_SOLVER_INSTALLED = False
 
 TEST_SUDOKU_FILES_DIRECTORY = './sudoku_data'
 
 SOLUTION_TYPE_BACKTRACKING = 0
 SOLUTION_TYPE_SAT_SOLVER = 1
 
+# Temp files to be used if `minisat` command line tool is utilized.
+SUDOKU_CNF_FILE = 'del.cnf'	
+MINISAT_RESULT_FILE = 'sol.txt'	
+OUTPUT_BUFFER = 'del.txt'
+
+PRINT_SPACE = '          '
+
 # Tidy print constants
 CHECK_MARK = '\033[92m' + '\033[1m' + u'\u2713' + '\033[0m' # Green + Bold + CheckMark + EndColor
 FAIL_MARK = '\033[91m' + '\033[1m' + 'X' + '\033[0m' # Red + Bold + X + EndColor
-
 
 NUM_FILES_TO_RUN_TESTS_ON = 45
 
@@ -25,6 +44,7 @@ class Sudoku:
     def __init__(self, dim = 9):
         self.dim = dim
         self.sat_solver = Glucose3()
+        self.minisat_clauses = []
         self.possible_values = [(i + 1) for i in range(self.dim)]
         # Flattened representation of the Sudoku Board.
         self.backing_array = [0 for i in range(self.dim * self.dim)]
@@ -97,7 +117,6 @@ class Sudoku:
             capture.append(to_print)
             if (row + 1) % sqrt == 0:
                 capture.append(top_bottom_rows)
-        capture.append('')
 
         return capture
     
@@ -107,7 +126,10 @@ class Sudoku:
     
     def print_old_and_new_captures(self, oldc, newc):
         for i in range(len(oldc)):
-            print(oldc[i] + '\t' + newc[i])
+            if i == 6: # Middle section...print arrow.
+                print(oldc[i] + '=========>' + newc[i])
+            else:
+                print(oldc[i] + PRINT_SPACE + newc[i])
 
     # Returns the subsudoku where the cell (x,y) is located in.
     def get_subsudoku(self, x, y):
@@ -283,20 +305,19 @@ class Sudoku:
 
     # Takes a 2D representation of a sudoku board and prints its solution.
     def generate_cnf_clauses(self):
-        clauses = []
         # Make sure that the pre-filled clues are considered (enforced)...
         for the_id in range(self.dim * self.dim):
             num = self.get_for_id(the_id)
             if num is not None and num != 0:
                 x, y = self.get_coord_for_id(the_id)
-                self.sat_solver.add_clause([self.code(num, x, y)])
+                self.add_clause([self.code(num, x, y)])
         
         # Enforce only 1 entry of all the entries that can go to a cell is included.
         for row in range(self.dim):
             for col in range(self.dim):
                 for i in range(1,self.dim):
                     for j in range(2,self.dim + 1):
-                        self.sat_solver.add_clause([-i, -j])
+                        self.add_clause([-i, -j])
 
 
         # Enforce at least one of all the entries of every number that can go to a cell is included.
@@ -306,7 +327,7 @@ class Sudoku:
                 for num in range(1,self.dim + 1):
                     num_code = self.code(num, row, col)
                     clause.append(num_code)
-                self.sat_solver.add_clause(clause)
+                self.add_clause(clause)
         
         sqrt = int(math.sqrt(self.dim))
         # Enforce no repeat of any number within a 3x3 subsudoku.
@@ -323,7 +344,7 @@ class Sudoku:
                     
                     for i in range(len(grid3x3) - 1):
                         for j in range(i + 1, len(grid3x3)):
-                            self.sat_solver.add_clause([-grid3x3[i], -grid3x3[j]])
+                            self.add_clause([-grid3x3[i], -grid3x3[j]])
                     
         for num in range(1, self.dim + 1):
             for row in range(self.dim):
@@ -345,27 +366,58 @@ class Sudoku:
                 # Enforce no repeatition of any number within a row.
                 for i in range(len(row_elems) - 1):
                     for j in range(i + 1, len(row_elems)):
-                        self.sat_solver.add_clause([-row_elems[i], -row_elems[j]])
+                        self.add_clause([-row_elems[i], -row_elems[j]])
 
                 # Enforce that a number appears at least once in a column.
                 clause = []
                 for elem in col_elems:
                     clause.append(elem)
-                self.sat_solver.add_clause(clause)
+                self.add_clause(clause)
 
                 # Enforce no repeatition of any number within a column.
                 for i in range(len(col_elems) - 1):
                     for j in range(i + 1, len(col_elems)):
-                        self.sat_solver.add_clause([-col_elems[i], -col_elems[j]])
-        
+                        self.add_clause([-col_elems[i], -col_elems[j]])
 
-    def solve_minisat(self):
+    def add_clause(self, clause):
+        if PYSAT_SOLVER_INSTALLED:
+            self.sat_solver.add_clause(clause)
+        else:
+            self.minisat_clauses.append(' '.join([str(x) for x in clause]) + ' 0')
+
+    def write_clauses_to_file(self):	
+        cnf = open(SUDOKU_CNF_FILE, 'w')	
+        sqrt = int(math.sqrt(self.dim))	
+        num_variables = (self.dim**sqrt)	
+
+        cnf.write('p cnf %d %d\n' % (num_variables, len(self.minisat_clauses)))	
+        for clause in self.minisat_clauses:	
+            cnf.write(clause + '\n')	
+        cnf.close()	
+
+    # Runs the minisat command line tool.
+    def run_minisat(self):	
+        system("minisat %s %s > %s"%(SUDOKU_CNF_FILE, MINISAT_RESULT_FILE, OUTPUT_BUFFER))	
+        return [int(x) for x in (open(MINISAT_RESULT_FILE, 'r').readlines()[1].split())]
+    
+    def solve_sat_solver(self):
         self.save_old_copy()
         self.generate_cnf_clauses()
-        if not self.sat_solver.solve():
-            return False
-        minisat_solution = self.sat_solver.get_model()
-        selected_cells = [x for x in minisat_solution if x > 0]
+        solution = []
+
+        if PYSAT_SOLVER_INSTALLED:
+            if not self.sat_solver.solve():
+                return False
+            solution = self.sat_solver.get_model()
+        else:
+            self.write_clauses_to_file()
+            solution = self.run_minisat()
+
+            system("rm %s"%SUDOKU_CNF_FILE)
+            system("rm %s"%MINISAT_RESULT_FILE)
+            system("rm %s"%OUTPUT_BUFFER)
+
+        selected_cells = [x for x in solution if x > 0]
         
         for i in selected_cells:
             num, row, column = self.decode(i)        
@@ -391,7 +443,7 @@ def solve_sudoku(sudoku_board, solution_type):
     if solution_type == SOLUTION_TYPE_BACKTRACKING:
         success = sudoku.solve_backtrack()
     elif solution_type == SOLUTION_TYPE_SAT_SOLVER:
-        success = sudoku.solve_minisat()
+        success = sudoku.solve_sat_solver()
 
     if success:
         assert (sudoku.check())
@@ -471,6 +523,13 @@ def run_tests(solution_type, num_files_to_execute):
         
     total_time_taken = time.time() - start_time
     average_solve_time = (total_time_taken)/solved_sudokus_count
+
+    solution_type_str = ''
+    if solution_type == SOLUTION_TYPE_BACKTRACKING:
+        solution_type_str = 'BACKTRACKING'
+    else:
+        solution_type_str = 'SAT SOLVER'
+    print('\033[1m' + 'ANALYSIS FOR ' + solution_type_str + ' SOLUTION.' + '\033[0m')
     print("Total time taken for %d sudokus: %ss." % (solved_sudokus_count, round(total_time_taken, 3)))
     print("Failed solve attempts: %d" % (NUM_FILES_TO_RUN_TESTS_ON - solved_sudokus_count))
     print("Average time taken per sudoku: %ss." % (round(average_solve_time, 3)))
